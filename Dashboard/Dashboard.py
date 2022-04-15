e9a49d30d72b9fa04c38af76b552e9571418d964
Popov,Yoan-Asen Y.B <y.popov@student.fontys.nl>
2022-04-15 21:46:02 +0200

    Added disk_info and network_info

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from tkinter import *
 import psutil
 import platform
@@ -6,6 +7,8 @@ import cpuinfo
 import socket
 import uuid
 import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 from requests import options
 
@@ -40,50 +43,65 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 
-def disks():
+def disks(window = 0):
     # disk_general = ['disk1_lbl', 'disk2_lbl', 'disk3_lbl', 'disk4_lbl', 'disk5_lbl','disk6_lbl']
     # disk_more = ['disk1_lbl', 'disk2_lbl', 'disk3_lbl', 'disk4_lbl', 'disk5_lbl','disk6_lbl']
     partitions = psutil.disk_partitions()
     # count = 0
     rows1 = 9
     rows2 = 10
+    rows3_more = 0
     for partition in partitions:
-        Label(frame_genral, text=f"Device: {partition.device}\nMountpoint: {partition.mountpoint}\nFile system type: {partition.fstype}").grid(row=rows1, column=0, sticky='w')
+        
         try:
             partition_usage = psutil.disk_usage(partition.mountpoint)
         except PermissionError:
             # this can be catched due to the disk that
             # isn't ready
             continue
-        Label(frame_genral, text=f"  Total Size: {get_size(psutil.disk_usage(partition.mountpoint).total)}").grid(row=rows2, column=0, sticky='w')
-        print(f"  Used: {get_size(partition_usage.used)}")
-        print(f"  Free: {get_size(partition_usage.free)}")
-        print(f"  Percentage: {partition_usage.percent}%")
-        disk_io = psutil.disk_io_counters()
-        print(f"Total read: {get_size(disk_io.read_bytes)}")
-        print(f"Total write: {get_size(disk_io.write_bytes)}")
         
+        if window == 1:
+            Label(frame_genral, text=f"Device: {partition.device}").grid(row=rows3_more, column=0, sticky='w')
+            Label(frame_genral, text=f"Mountpoint: {partition.mountpoint}").grid(row=rows3_more+1, column=0, sticky='w')
+            Label(frame_genral, text=f"File system type: {partition.fstype}").grid(row=rows3_more+2, column=0, sticky='w')
+            Label(frame_genral, text=f"Total Size: {get_size(psutil.disk_usage(partition.mountpoint).total)}").grid(row=rows3_more, column=1, sticky='w')
+            #k=LabelFrame(frame_genral, text="Info").grid(row=rows2+1, column=0, sticky='w')
+            Label(frame_genral, text=f"Used: {get_size(partition_usage.used)}").grid(row=rows3_more+1, column=1, sticky='w')
+            Label(frame_genral, text=f"Free: {get_size(partition_usage.free)}").grid(row=rows3_more+2, column=1, sticky='w')
+            Label(frame_genral, text=f"Percentage: {partition_usage.percent}%").grid(row=rows3_more+3, column=1, sticky='w')
+            # get IO statistics since boot
+            disk_io = psutil.disk_io_counters()
+            Label(frame_genral, text=f"Total read: {get_size(disk_io.read_bytes)}").grid(row=rows3_more+4, column=1, sticky='w')
+            Label(frame_genral, text=f"Total write: {get_size(disk_io.write_bytes)} \n").grid(row=rows3_more+5, column=1, sticky='w')
+        else:
+            Label(frame_genral, text=f"Device: {partition.device}").grid(row=rows1, column=0, sticky='w')
+            Label(frame_genral, text=f"Total Size: {get_size(psutil.disk_usage(partition.mountpoint).total)}").grid(row=rows2, column=0, sticky='w')
         rows1 += 2
         rows2 += 2
 
+        rows3_more += 6
+
 
 def net():
+    rows = 0
     if_addrs = psutil.net_if_addrs()
     for interface_name, interface_addresses in if_addrs.items():
         for address in interface_addresses:
-            print(f"=== Interface: {interface_name} ===")
+            Label(frame_genral, text=f"=== Interface: {interface_name} ===").grid(row=rows, column=1, sticky='w')
             if str(address.family) == 'AddressFamily.AF_INET':
-                print(f"  IP Address: {address.address}")
-                print(f"  Netmask: {address.netmask}")
-                print(f"  Broadcast IP: {address.broadcast}")
+                Label(frame_genral, text=f"  IP Address: {address.address}").grid(row=rows+1, column=1, sticky='w')
+                Label(frame_genral, text=f"  Netmask: {address.netmask}").grid(row=rows+2, column=1, sticky='w')
+                Label(frame_genral, text=f"  Broadcast IP: {address.broadcast}").grid(row=rows+3, column=1, sticky='w')
             elif str(address.family) == 'AddressFamily.AF_PACKET':
-                print(f"  MAC Address: {address.address}")
-                print(f"  Netmask: {address.netmask}")
-                print(f"  Broadcast MAC: {address.broadcast}")
+                Label(frame_genral, text=f"  MAC Address: {address.address}").grid(row=rows+4, column=1, sticky='w')
+                Label(frame_genral, text=f"  Netmask: {address.netmask}").grid(row=rows+5, column=1, sticky='w')
+                Label(frame_genral, text=f"  Broadcast MAC: {address.broadcast}").grid(row=rows+6, column=1, sticky='w')
+
+        rows += 7
     ##get IO statistics since boot
-    net_io = psutil.net_io_counters()
-    print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
-    print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
+    # net_io = psutil.net_io_counters()
+    # Label(frame_genral, text=f"Total Bytes Sent: {get_size(net_io.bytes_sent)}").grid(row=rows+7, column=1, sticky='w')
+    # Label(frame_genral, text=f"Total Bytes Received: {get_size(net_io.bytes_recv)}").grid(row=rows+8, column=1, sticky='w')
 
 
 def clear_frame():
@@ -146,41 +164,46 @@ def genral_info():
         disks()
         #net()
         global live_update
-        live_update = frame_genral.after(750, clock)
+        live_update = frame_genral.after(769, clock)
     clock()
 
 
 def cpu_info():
-    countert = 8
-    processor_name_lbl = Label(frame_genral, text=f"Processor: {cpuinfo.get_cpu_info()['brand_raw']}")
-    cores_physical_lbl = Label(frame_genral, text=f"Physical cores: {psutil.cpu_count(logical=False)}")
-    cores_lbl = Label(frame_genral, text=f"All cores: {psutil.cpu_count(logical=True)}")
-    cpu_lbl = Label(frame_genral, text=f"CPU usage: {psutil.cpu_percent()}%")
-    cpufrq_lbl = Label(frame_genral, text=f"Current Frequency: {psutil.cpu_freq().min:.2f}Mhz")
-    cpufrq_max_lbl = Label(frame_genral, text=f"Max Frequency: {cpufreq.max:.2f}Mhz")
-    cpufrq_min_lbl = Label(frame_genral, text=f"Min Frequency: {cpufreq.min:.2f}Mhz")
-    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-        Label(frame_genral, text=f"Core {i}: {percentage}%").grid(row=countert, column=0, sticky='w')
-        countert += 1
-
-    processor_name_lbl.grid(row=1, column=0, sticky='w')
-    cores_physical_lbl.grid(row=2, column=0, sticky='w')
-    cores_lbl.grid(row=3, column=0, sticky='w')
-    cpu_lbl.grid(row=4, column=0, sticky='w')
-    cpufrq_lbl.grid(row=5, column=0, sticky='w')
-    cpufrq_max_lbl.grid(row=6, column=0, sticky='w')
-    cpufrq_min_lbl.grid(row=7, column=0, sticky='w')
 
     def clock():
+        clear_frame()
+
+
         countert = 8
-        k = psutil.cpu_freq()
-        cpu = f"CPU usage: {psutil.cpu_percent()}%"
-        frq = f"Current Frequency: {k.current:.2f}Mhz"
-        cpu_lbl.config(text=cpu)
-        cpufrq_lbl.config(text=frq)
+        processor_name_lbl = Label(frame_genral, text=f"Processor: {cpuinfo.get_cpu_info()['brand_raw']}")
+        cores_physical_lbl = Label(frame_genral, text=f"Physical cores: {psutil.cpu_count(logical=False)}")
+        cores_lbl = Label(frame_genral, text=f"All cores: {psutil.cpu_count(logical=True)}")
+        cpu_lbl = Label(frame_genral, text=f"CPU usage: {psutil.cpu_percent()}%")
+        cpufrq_lbl = Label(frame_genral, text=f"Current Frequency: {psutil.cpu_freq().min:.2f}Mhz")
+        cpufrq_max_lbl = Label(frame_genral, text=f"Max Frequency: {cpufreq.max:.2f}Mhz")
+        cpufrq_min_lbl = Label(frame_genral, text=f"Min Frequency: {cpufreq.min:.2f}Mhz")
         for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
             Label(frame_genral, text=f"Core {i}: {percentage}%").grid(row=countert, column=0, sticky='w')
             countert += 1
+
+        processor_name_lbl.grid(row=1, column=0, sticky='w')
+        cores_physical_lbl.grid(row=2, column=0, sticky='w')
+        cores_lbl.grid(row=3, column=0, sticky='w')
+        cpu_lbl.grid(row=4, column=0, sticky='w')
+        cpufrq_lbl.grid(row=5, column=0, sticky='w')
+        cpufrq_max_lbl.grid(row=6, column=0, sticky='w')
+        cpufrq_min_lbl.grid(row=7, column=0, sticky='w')
+
+    
+        # countert = 8
+        # k = psutil.cpu_freq()
+        # cpu = f"CPU usage: {psutil.cpu_percent()}%"
+        # frq = f"Current Frequency: {k.current:.2f}Mhz"
+        # cpu_lbl.config(text=cpu)
+        # cpufrq_lbl.config(text=frq)
+        # for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+        #     Label(frame_genral, text=f"Core {i}: {percentage}%").grid(row=countert, column=0, sticky='w')
+        #     countert += 1
         global live_update
         live_update = frame_genral.after(1000, clock)
     clock()
@@ -209,11 +232,23 @@ def ram_info():
 
 
 def disk_info():
-    disks()
+    def clock():
+        clear_frame()
+        disks(1)
+        global live_update
+        live_update = frame_genral.after(6900, clock)
+    clock()
+    
 
 
 def network_info():
-    pass
+    def clock():
+        clear_frame()
+        net()
+        global live_update
+        live_update = frame_genral.after(6900, clock)
+    clock()
+    
 
 option = ["Genral Info", "CPU", "RAM", "Disk", "Network"]
 clicked = StringVar()
